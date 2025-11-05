08053025497b08b2a0bb4791de4a24345d5e4fa9
sofiaroman1969 <maria.roman@tecdesoftware.edu.mx>
2025-11-05 08:29:23 -0600

    Ejercicio 1 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -15,9 +15,9 @@ function miInformacion() {
   // TODO: Declara las variables nombre, edad y carrera
   // Ejemplo: const nombre = "Juan";
   
-  const nombre = "";
-  const edad = 0;
-  const carrera = "";
+  const nombre = "Sofia";
+  const edad = 19;
+  const carrera = "TSU";
   
   return { nombre, edad, carrera };
 }
