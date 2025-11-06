1572d48005e232932d4e0ec07ca0a2f7f1c5926a
sofiaroman1969 <maria.roman@tecdesoftware.edu.mx>
2025-11-06 12:11:33 -0600

    Ejercicio 17 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -278,7 +278,10 @@ function filtrarMayores(numeros, limite) {
 function invertirArray(arr) {
   // TODO: Invierte el orden de los elementos
   // Pista: Puedes usar arr.reverse() o hacerlo manualmente
-  const invertido = [];
+const invertido = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+      invertido.push(arr[i]);
+  }
   return invertido;
 }
 
